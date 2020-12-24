fea26f1556e07dc85d87ab0f6e2bbd6daf890b8b
Dominik <215481@edu.p.lodz.pl>
2020-12-25 00:02:38 +0100

    I HOPE ITS GONNAdad WORK!w

--- src/main/java/EncryptionManager.java
+++ src/main/java/EncryptionManager.java
@@ -7,9 +7,9 @@ import java.security.NoSuchAlgorithmException;
 
 public class EncryptionManager {
     private static final String ALGO = "AES";
-    private static final byte[] keyValue = "Ad0#2s!3oGyRq!5F".getBytes();
+   public static final byte[] keyValue = "Ad0#2s!3oGyRq!5F".getBytes();
     public static byte[] decrypt(byte[] encryptedData) throws Exception {
-        Key key = new SecretKeySpec(keyValue, ALGO);
+        Key key = new SecretKeySpec(keyValue, "AES/ECB/PKCS5Padding");
         Cipher c = Cipher.getInstance(ALGO);
         c.init(Cipher.DECRYPT_MODE, key);
 
@@ -17,7 +17,7 @@ public class EncryptionManager {
         return decValue;
     }
     public static byte[] encrypt(byte[] Data) throws Exception {
-        Key key = new SecretKeySpec(keyValue, ALGO);
+        Key key = new SecretKeySpec(keyValue, "AES/ECB/PKCS5Padding");
         Cipher c = Cipher.getInstance(ALGO);
         c.init(Cipher.ENCRYPT_MODE, key);
         byte[] encVal = c.doFinal(Data);
