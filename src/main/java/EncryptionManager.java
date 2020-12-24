a7f9da167c2b6aae10975920f6027af42b4ef16b
Dominik <215481@edu.p.lodz.pl>
2020-12-25 00:09:34 +0100

    I HOPE ITS GONNAdad WORK!w

--- src/main/java/EncryptionManager.java
+++ src/main/java/EncryptionManager.java
@@ -1,27 +1,65 @@
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class EncryptionManager {
-    private static final String ALGO = "AES";
-   public static final byte[] keyValue = "Ad0#2s!3oGyRq!5F".getBytes();
-    public static byte[] decrypt(byte[] encryptedData) throws Exception {
-        Key key = new SecretKeySpec(keyValue, "AES/ECB/PKCS5Padding");
-        Cipher c = Cipher.getInstance(ALGO);
-        c.init(Cipher.DECRYPT_MODE, key);
-
-        byte[] decValue = c.doFinal(encryptedData);
-        return decValue;
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+    private static final String secret = "TADAELOWMISIEMUA";
+    public static void setKey(String myKey)
+    {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
-    public static byte[] encrypt(byte[] Data) throws Exception {
-        Key key = new SecretKeySpec(keyValue, "AES/ECB/PKCS5Padding");
-        Cipher c = Cipher.getInstance(ALGO);
-        c.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encVal = c.doFinal(Data);
-        //String encryptedValue = new BASE64Encoder().encode(encVal);
-        return encVal;
+
+    public static byte[] encrypt(byte[] strToEncrypt)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return cipher.doFinal(strToEncrypt);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public static byte[] decrypt(byte[] strToDecrypt)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return cipher.doFinal(Base64.getDecoder().decode(strToDecrypt));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
     }
 }
