ca77720778fd9ffe29891efe22d358a8ba53d9b5
Dominik <215481@edu.p.lodz.pl>
2020-12-29 12:10:48 +0100

    UHU!!!

--- src/main/java/EncryptionManager.java
+++ src/main/java/EncryptionManager.java
@@ -3,10 +3,7 @@ import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.ByteBuffer;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 
@@ -16,100 +13,23 @@ import java.security.spec.KeySpec;
  */
 public class EncryptionManager {
 
-    /**
-     * This method will encrypt the given data
-     * @param data : the data that will be encrypted
-     * @return Encrypted data in a byte array
-     */
-    public static byte [] encrypt( byte [] data) throws NoSuchPaddingException,
-            NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException,
-            InvalidKeyException,
-            BadPaddingException,
-            IllegalBlockSizeException, InvalidKeySpecException {
+    private static final String ALGO = "AES";
+    private static final byte[] keyValue = "Ad0#2s!3oGyRq!5F".getBytes();
 
-        //Prepare the nonce
-        SecureRandom secureRandom = new SecureRandom();
-        String key = "Bar12345Bar12345";
-        //Noonce should be 12 bytes
-        byte[] iv = new byte[12];
-        secureRandom.nextBytes(iv);
-
-        //Prepare your key/password
-        SecretKey secretKey = generateSecretKey(key, iv);
-
-
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv);
-
-        //Encryption mode on!
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
-
-        //Encrypt the data
-        byte [] encryptedData = cipher.doFinal(data);
-
-        //Concatenate everything and return the final data
-        ByteBuffer byteBuffer = ByteBuffer.allocate(4 + iv.length + encryptedData.length);
-        byteBuffer.putInt(iv.length);
-        byteBuffer.put(iv);
-        byteBuffer.put(encryptedData);
-        return byteBuffer.array();
+    public static byte[] encrypt(byte[] Data) throws Exception {
+        Key key = new SecretKeySpec(keyValue, ALGO);
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(Data);
+        //String encryptedValue = new BASE64Encoder().encode(encVal);
+        return encVal;
     }
+    public static byte[] decrypt(byte[] encryptedData) throws Exception {
+        Key key = new SecretKeySpec(keyValue, ALGO);
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.DECRYPT_MODE, key);
 
-
-    public static byte [] decrypt(byte [] encryptedData)
-            throws NoSuchPaddingException,
-            NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException,
-            InvalidKeyException,
-            BadPaddingException,
-            IllegalBlockSizeException,
-            InvalidKeySpecException {
-
-
-        //Wrap the data into a byte buffer to ease the reading process
-        ByteBuffer byteBuffer = ByteBuffer.wrap(encryptedData);
-        String key = "Bar12345Bar12345";
-        int noonceSize = byteBuffer.getInt();
-
-        //Make sure that the file was encrypted properly
-        byte[] iv = new byte[noonceSize];
-        byteBuffer.get(iv);
-
-        //Prepare your key/password
-        SecretKey secretKey = generateSecretKey(key, iv);
-
-        //get the rest of encrypted data
-        byte[] cipherBytes = new byte[byteBuffer.remaining()];
-        byteBuffer.get(cipherBytes);
-
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv);
-
-        //Encryption mode on!
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
-
-        //Encrypt the data
-        return cipher.doFinal(cipherBytes);
-
+        byte[] decValue = c.doFinal(encryptedData);
+        return decValue;
     }
-
-    /**
-     * Function to generate a 128 bit key from the given password and iv
-     * @param password
-     * @param iv
-     * @return Secret key
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeySpecException
-     */
-    public static SecretKey generateSecretKey(String password, byte [] iv) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), "ssshhhhhhhhhhh!!!!".getBytes(), 65536, 128); // AES-128
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        byte[] key = secretKeyFactory.generateSecret(spec).getEncoded();
-        return new SecretKeySpec(key, "AES");
-    }
-    public static int sizeOfEncrypted(int i ) throws NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeySpecException {
-        return encrypt(new byte [i]).length;
-    }
-
 }
